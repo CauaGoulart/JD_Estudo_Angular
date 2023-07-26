971333b66c08a27192f85585f55c3dcff5b12118
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-07-26 09:48:05 -0300

    Update Extra

--- src/app/ng-style/ng-style.component.ts
+++ src/app/ng-style/ng-style.component.ts
@@ -7,17 +7,29 @@ import { Component } from '@angular/core';
 })
 export class NgStyleComponent {
 
-  public largura : number = 100;
-  public larguraStr : string = "100px"
+  public largura: number = 100;
+  public larguraStr: string = "100px";
+  public altura: number = 250;
+  public alturaStr: string = "250px";
 
-  aumenta(){
+  aumentaCima() {
+    this.altura -= 50;
+    this.alturaStr = this.altura + "px";
+  }
+
+  diminuiBaixo() {
+    this.altura += 50;
+    this.alturaStr = this.altura + "px";
+  }
+
+  aumentaDireita() {
     this.largura += 50;
-    this.larguraStr = this.largura+"px"
+    this.larguraStr = this.largura + "px";
   }
 
-  diminui(){
+  diminuiEsquerda() {
     this.largura -= 50;
-    this.larguraStr = this.largura+"px"
+    this.larguraStr = this.largura + "px";
   }
-
 }
+
